fc3c418a8a2c9492db703b03626d6a1c7e387826
antman666 <945360554@qq.com>
2021-11-26 23:21:37 +0800

    update something

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,11 @@ cat << EOM >> /etc/pacman.conf
 Include = /etc/pacman.d/mirrorlist
 EOM
 
-pacman -Syu --noconfirm --needed base-devel pacman-contrib
+pacman -Syu --noconfirm --needed base-devel pacman-contrib wget
 
-pacman -U --noconfirm --needed https://github.com/antman666/own_repo/releases/download/x86_64/gcc-11.2.0-1-x86_64.pkg.tar.zst
+wget -c https://github.com/antman666/own_repo/releases/download/x86_64/gcc-11.2.0-1-x86_64.pkg.tar.zst
+
+pacman -U --noconfirm --needed gcc-11.2.0-1-x86_64.pkg.tar.zst
 
 # Makepkg does not allow running as root
 # Create a new user `builder`
