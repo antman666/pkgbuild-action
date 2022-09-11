06a0b3b6a0aa56395e2e0a0def617babe6c37206
antman666 <945360554@qq.com>
2022-09-11 10:00:25 +0800

    update repo

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ cat << EOM >> /etc/pacman.conf
 Include = /etc/pacman.d/mirrorlist
 EOM
 
-pacman -Syu --noconfirm --needed base-devel pacman-contrib wget
+pacman -Syu --noconfirm --needed base-devel pacman-contrib wget ccache
 # wget -qc https://www.archlinux.club/x86_64/llvm+clang-14.0.6-2-x86_64.pkg.tar.zst
 # wget -qc https://www.archlinux.club/x86_64/libclc+clang-14.0.6-1-any.pkg.tar.zst
 # wget -c https://www.archlinux.club/x86_64/jemalloc+clang-1:5.3.0-1-x86_64.pkg.tar.zst
