a053a016241734ad2e19ae675f82938973fd627d
antman666 <62091572+antman666@users.noreply.github.com>
2023-06-02 06:16:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ FILE="$(basename "$0")"
 # Include = /etc/pacman.d/mirrorlist
 # EOM
 
-pacman -Syu --noconfirm --needed base-devel pacman-contrib wget mimalloc jemalloc
+pacman -Syu --noconfirm --needed base-devel pacman-contrib wget mimalloc jemalloc llvm
 
 # Makepkg does not allow running as root
 # Create a new user `builder`
